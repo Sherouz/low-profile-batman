1df2652e09c30c812d051d66e0eae7f676f4dd2a
Shahrouz <sherouz@proton.me>
2025-12-15 23:57:25 +0330

    Updated main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from src.state import init_state, clamp_state, is_game_over
 from src.actions import apply_action
 from src.ui import display_state, get_player_action
 from src.result import show_result
+
 def run_game():
     """
     Main game loop.
